d458b736aa3cc690636bcda0be86fba4bd54f69a
2cpk-fin <prokayminer@gmail.com>
2026-03-11 14:05:07 +0700

    Initial commit of the project

---
+++ xhdh/xhdh/src/main/java/com/xhdh/xhdh/XhdhApplication.java
@@ -0,0 +1,13 @@
+package com.xhdh.xhdh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhdhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XhdhApplication.class, args);
+	}
+
+}
