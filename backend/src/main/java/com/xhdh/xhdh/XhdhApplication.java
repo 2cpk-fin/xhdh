4accc920a6a7bc242484a6a25787b105f57deea0
LKLD1909 <codeboy1909@gmail.com>
2026-03-31 15:30:20 +0700

    Update

--- backend/src/main/java/com/xhdh/xhdh/XhdhApplication.java
+++ backend/src/main/java/com/xhdh/xhdh/XhdhApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -22,11 +20,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class XhdhApplication {
 
 	public static void main(String[] args) {
-		Dotenv.configure()
-		.ignoreIfMissing()
-		.systemProperties()
-		.load();
-		System.out.println("JWT_SECRET từ hệ thống: " + System.getenv("JWT_SECRET"));
 		SpringApplication.run(XhdhApplication.class, args);
 	}
 
